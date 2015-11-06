5cbf747a91594fb779689f7e708318e5cf647674
nick-benoit14 <nick.benoit14@gmail.com>
2015-11-05 20:55:45 -0700

    sidebar

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -1,67 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-
-<script>
-jQuery(document).ready(function()
-		{
-
-			 /* OnMouseOver Animation  */
-
-						$(".group",this).mouseenter(
-										function()
-												{
-														$("div.details", this).animate({"bottom":'-' + $("div.details",this).height() + 'px'}, 200);
-														$(".group.photocontainer", this).addClass("selected-photocontainer");
-														$(".group.details", this).addClass("selected-details");
-												});
-
-						$(".group",this).mouseleave(
-										function()
-												{
-													 $("div.details", this).animate({"bottom":'-' + $("div.title",this).height() + 'px'}, 200);
-														$(".group.photocontainer", this).removeClass("selected-photocontainer");
-														$(".group.details", this).removeClass("selected-details");
-
-												});
-
-		$(window).resize(function()
-							 {
-									 if($(window).width() < 1100)
-											 {
-
-												 //minimize slider
-													 $('.cachemakers.cache-slider-container-full').removeClass('cache-slider-container-full')
-													 .addClass('cache-slider-container-min');
-
-														$('.cachemakers.cache-announcement-text-full').removeClass('cachemakers cache-announcement-text-full')
-													 .addClass('cachemakers cache-announcement-text-min');
-
-												 //Minimize blockwrapper  <div class="blockwrapper-min">
-													 $('.blockwrapper').removeClass('blockwrapper').addClass('blockwrapper-min');
-
-												 //minimize sidebar <div class="cacheonium cache-sidebar-min grey">
-													 $('.cacheonium.cache-sidebar-full').removeClass('cache-sidebar-full').addClass('cache-sidebar-min');
-
-
-											 }
-									 else
-											{
-													 $('.cachemakers.cache-slider-container-min').removeClass('cache-slider-container-min') //maximize slider
-													 .addClass('cache-slider-container-full');
-
-														$('.cachemakers.cache-announcement-text-min').removeClass('cachemakers cache-announcement-text-min')
-													 .addClass('cachemakers cache-announcement-text-full');
-
-												//Minimize blockwrapper  <div class="blockwrapper-min">
-													 $('.blockwrapper-min').removeClass('blockwrapper-min').addClass('blockwrapper');
-
-												 //minimize sidebar <div class="cacheonium cache-sidebar-min grey">
-													 $('.cacheonium.cache-sidebar-min').removeClass('cache-sidebar-min').addClass('cache-sidebar-full');
-											}
-							 });
-			 });
-</script>
-
 <style>
 .red{background-color:#ff3722; color:white; font-size:18px}
 .cachemakers-block{height:auto; width:250px;display:inline-block;
@@ -80,7 +17,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<header class="page-header">
 				<h1 class="page-title">
-					<?php the_title(); ?>
+					<?php// the_title(); ?>
 				</h1>
 			</header><!-- .page-header -->
 
@@ -132,8 +69,6 @@ get_header(); ?>
 			?>
 
 		</main><!-- #main -->
-		<?php get_sidebar(); ?>
-
 	</div><!-- #primary -->
-
-<?php get_footer(); ?>
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
