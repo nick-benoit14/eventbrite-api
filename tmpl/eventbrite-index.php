8dad9b3f7c525b6f41606acc5f91ca24bdb37249
nick-benoit14 <nick.benoit14@gmail.com>
2015-12-27 18:33:05 -0700

    css

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -1,7 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <style>
 	.cachemakers-header {padding:4%; text-align:center; }
-	.cachemakers-header h1 p{ text-align:center; width:66%; margin-left:auto; margin-right:auto;}
+	.cachemakers-header h1 p{ text-align:center; width:66%; margin-left:auto; margin-right:auto; margin-bottom:2%}
 
 	.cachemakers-box{ max-width:950px; margin-left:auto; margin-right:auto; }
 	#main{width:100%; padding-left:10%; padding-right:10%;}
