b43965175a45d7eed4b8f5dd83d859b3d54bbc6f
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-06 15:42:01 -0700

    changed size

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -77,9 +77,9 @@ get_header(); ?>
 
 			<div class="cachemakers-header"><h1>Things We've Made</h1></div>
 			<div>
-				<iframe width="50%"  src="https://www.youtube.com/embed/MxaKxS5POmY" frameborder="0" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/MxaKxS5POmY" frameborder="0" allowfullscreen></iframe>
 			</div>
-			
+
 			<div class="cachemakers-header"><h1>Location</h1></div>
 			<div>
 				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1489.1541753032047!2d-111.83529
