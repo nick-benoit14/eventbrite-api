ab13af6eb2c6bb7d188671a938a59eeb0a8889d6
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-06 15:47:13 -0700

    margin

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -77,8 +77,8 @@ get_header(); ?>
 
 			<div class="cachemakers-header"><h1>Things We've Made</h1></div>
 			<div>
-				<iframe style="float:left;" width="560" height="315" src="https://www.youtube.com/embed/MxaKxS5POmY" frameborder="0" allowfullscreen></iframe>
-				<iframe style="float:right;" width="560" height="315" src="https://www.youtube.com/embed/CYzd85A-oCE" frameborder="0" allowfullscreen></iframe>
+				<iframe style="float:left; margin:2%;" width="560" height="315" src="https://www.youtube.com/embed/MxaKxS5POmY" frameborder="0" allowfullscreen></iframe>
+				<iframe style="float:right; margin:2%;" width="560" height="315" src="https://www.youtube.com/embed/CYzd85A-oCE" frameborder="0" allowfullscreen></iframe>
 			</div>
 
 			<div class="cachemakers-header"><h1>Location</h1></div>
