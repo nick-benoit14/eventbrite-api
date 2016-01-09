9a746ef3113ffcf71f668ff47519f15da4f624f9
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-09 10:13:43 -0700

    moved index

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -1,3 +1,5 @@
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <style>
 	.cachemakers-header {padding:4%; text-align:center; }
@@ -7,6 +9,7 @@
 	#main{width:100%; padding-left:10%; padding-right:10%;}
 	iframe{ display:block; margin:auto !important; padding:2%; }
 </style>
+
 
 <?php
 /**
@@ -33,26 +36,6 @@ get_header(); ?>
 			</div>
 			<div style="margin:5%;">
 				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/114910479@N08/albums/72157661959120016" title="Website Slides"><img src="https://farm1.staticflickr.com/697/20499472514_61c1370293_b.jpg" style="display:block; margin-auto;" width="1024" height="768" alt="Website Slides"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
-			</div>
-			<div class="cachemakers-header"><h1>Location</h1></div>
-			<div>
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1489.1541753032047!2d-111.83529
-				941880711!3d41.71386464291981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87547e380e50b5e9%
-				3A0xd48d3e368581ac3a!2s990+S+Main+St%2C+Logan%2C+UT+84321!5e0!3m2!1sen!2sus!4v1449011906529" width="400"
-				height="300" frameborder="0" style="border:0; display:inline-block" allowfullscreen></iframe>
-
-				<div style="display:inline-block; width:500px; vertical-align:top; margin-left:20px; margin-right:20px;">
-			  <p>
-				Cache Makerspace </br>
-				990 S Main Suite A</br>
-				Logan, UT
-		    </p>
-
-			  <p>
-					North of the Providence Maceys, east of the South Walmart, west of the Mattress Outlet, and directly south of (the former) Don Aslett. Right by the bus stop.
-			  </p>
-
-			</div>
 			</div>
 		</main><!-- #main -->
 	<?php// get_sidebar(); ?>
