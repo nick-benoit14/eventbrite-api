f9103ab72ea6ec4fd2a32655001db0193d8d5909
nick-benoit14 <nick.benoit14@gmail.com>
2016-01-10 17:46:49 -0700

    get the content

--- tmpl/eventbrite-single.php
+++ tmpl/eventbrite-single.php
@@ -33,7 +33,7 @@
 							</header><!-- .entry-header -->
 
 							<div class="entry-content post entry">
-								<p><?php the_content(); ?></p>
+								<p><?php get_the_content(); ?></p>
 
 								<?php eventbrite_ticket_form_widget(); ?>
 							</div><!-- .entry-content -->
